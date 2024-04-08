161a6cf2b90d1afc557c985b1dc9248806834bc7
sneh <sn372821@dal.ca>
2024-04-08 19:36:34 -0300

    sneh

---
+++ src/Pages/Admin/VideoUploadForm.js
@@ -0,0 +1,136 @@
+import React,{ useState } from 'react';
+import AWS from 'aws-sdk';
+import { useDropzone } from 'react-dropzone';
+import './VideoUploadForm.css';
+import useVideo from '../../Hooks/useVideo';
+import { toast } from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthContext';
+
+const VideoUploadForm = () => {
+  const { addVideo } = useVideo();
+  const [file, setFile] = useState(null);
+  const [thumbnail, setThumbnail] = useState(null);
+  const [title, setTitle] = useState('');
+  const navigate = useNavigate();
+  const {logout,setStatus} = React.useContext(AuthContext);
+
+  const { getRootProps: getVideoRootProps, getInputProps: getVideoInputProps } = useDropzone({
+    accept: 'video/*',
+    onDrop: (acceptedFiles) => {
+      setFile(acceptedFiles[0]);
+    },
+  });
+
+  const { getRootProps: getThumbnailRootProps, getInputProps: getThumbnailInputProps } = useDropzone({
+    accept: 'image/*',
+    onDrop: (acceptedFiles) => {
+      setThumbnail(acceptedFiles[0]);
+    },
+  });
+
+  const handleUpload = () => {
+    if (!file || !thumbnail || !title) {
+      console.error('Please provide all required fields');
+      return;
+    }
+
+    const s3 = new AWS.S3({
+      accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID, 
+      secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+      sessionToken: process.env.REACT_APP_AWS_SESSION_TOKEN,
+      region: 'us-east-1'
+    });
+
+    const videoParams = {
+      Bucket: 'anime-video-stream',
+      Key: file.name,
+      Body: file,
+    };
+
+    const thumbnailParams = {
+      Bucket: 'anime-video-stream',
+      Key: `thumbnails/${thumbnail.name}`,
+      Body: thumbnail,
+    };
+    toast.promise(
+    Promise.all([
+      s3.upload(videoParams).promise(),
+      s3.upload(thumbnailParams).promise(),
+    ])
+      .then((data) => {
+        console.log('Files uploaded successfully:', data);
+        addVideo({ title, vKey: data[0].Key, tKey: data[1].Key });
+        
+      })
+      .catch((err) => {
+        console.error('Error uploading files:', err);
+      }),{
+        loading: 'Uploading files...',
+        success: 'Files uploaded successfully',
+        error: 'Failed to upload files',
+      }
+    );
+  };
+
+  return (
+    <>
+      <header className="dark-mode">
+        <div className="logo-search">
+        <h1 className="dark-mode flicker">Aniverse</h1>
+        
+        </div>
+        <div className="user-actions">
+          <ul>
+            <li><Link to="/dashboard">Dashboard</Link></li>
+            <li><Link to="/upload">Upload</Link></li>
+            <li onClick={()=>{
+              logout();
+              setStatus(false);
+              navigate('/');
+            }}>Logout</li>
+            
+            </ul>
+    </div>
+      </header> 
+    <div className="upload-form">
+      <div className="form-group">
+        <input
+          type="text"
+          placeholder="Enter title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="form-input"
+          />
+      </div>
+      <div {...getVideoRootProps()} className="dropzone">
+        <input {...getVideoInputProps()} />
+        {file ? (
+          <div>
+            <p>{file.name}</p>
+            <video src={URL.createObjectURL(file)} controls />
+          </div>
+        ) : (
+          <p>Drag and drop video file here, or click to select</p>
+        )}
+      </div>
+      <div {...getThumbnailRootProps()} className="dropzone">
+        <input {...getThumbnailInputProps()} />
+        {thumbnail ? (
+          <div>
+            <p>{thumbnail.name}</p>
+            <img src={URL.createObjectURL(thumbnail)} alt="Thumbnail" />
+          </div>
+        ) : (
+          <p>Drag and drop thumbnail image here, or click to select</p>
+        )}
+      </div>
+      <button onClick={handleUpload} className="upload-button">
+        Upload
+      </button>
+    </div>
+        </>
+  );
+};
+
+export default VideoUploadForm;
---
+++ src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,140 @@
+import React, { useEffect, useState , useContext} from 'react';
+import './Dashboard.css';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthContext';
+import {useNavigate,useLocation} from 'react-router-dom';
+import useVideo from '../../Hooks/useVideo';
+import useAuth from '../../Hooks/useAuth';
+
+
+
+const Dashboard = () => {
+  const [videos, setVideos] = useState([]);
+  const {logout,setStatus} = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const {fetchVideos,searchVideo} = useVideo();
+  const [searchTerm, setSearchTerm] = useState('');
+  const {fetchUserDetails} = useAuth();
+
+  const [userDetails, setUserDetails] = useState({});
+
+  useEffect(() => {
+   const email = window.localStorage.getItem('email');
+   console.log('Email:',email);
+    fetchUserDetails({email: email}).then((data)=>{
+      console.log('Data:',data);
+    }).catch((err)=>{
+      console.log('Error:',err);
+    });
+    const fetchData = async () => {
+      try {
+        const data = await fetchVideos();
+        setVideos(data);
+      } catch (err) {
+        console.log('Error:', err);
+      }
+    };
+    fetchData();
+  }, []);
+
+
+  const handleThumbnailClick = (video) => {
+    setSelectedVideo(video);
+  };
+
+  const handleCloseVideo = () => {
+    setSelectedVideo(null);
+  };
+
+  const handleSearch = () => {
+    console.log('Search Term:',searchTerm);
+    const searchDetails = {
+      fieldValue: searchTerm
+    }
+      searchVideo(searchDetails).then((data)=>{
+      setVideos(data);
+      console.log('Data:',data);
+    }).catch((err)=>{
+      console.log('Error:',err);
+    });
+  };
+
+  return (
+    <>
+      <div className="App">
+        <header className="dark-mode">
+          <div className="logo-search">
+            <h1 className="dark-mode flicker">Aniverse</h1>
+            <input
+              className='search-bar'
+              type="text"
+              placeholder="Search movies..."
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSearch();
+                }
+              }}
+            />
+          </div>
+          <div className="user-actions">
+            <ul>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link to="/upload">Upload</Link>
+              </li>
+              <li
+                onClick={() => {
+                  logout();
+                  setStatus(false);
+                  navigate("/");
+                }}
+              >
+                Logout
+              </li>
+            </ul>
+          </div>
+        </header>
+        {selectedVideo && (
+          <div className="video-overlay">
+            <div className="video-player">
+              <video controls autoPlay>
+                <source
+                  src={`${process.env.REACT_APP_CLOUDFRONT}/${selectedVideo.vKey}`}
+                  type="video/mp4"
+                />
+                Your browser does not support the video tag.
+              </video>
+              <button onClick={handleCloseVideo}>Close</button>
+            </div>
+          </div>
+        )}
+        <main>
+          {videos.map((video, index) => (
+            <div
+              className="movie-container"
+              key={index}
+              onClick={() => handleThumbnailClick(video)}
+            >
+              <div className="movie-card">
+                <img
+                  src={`${process.env.REACT_APP_CLOUDFRONT}/${video.tKey}`}
+                  alt={video.title}
+                />
+                <h2>{video.title}</h2>
+              </div>
+            </div>
+          ))}
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
